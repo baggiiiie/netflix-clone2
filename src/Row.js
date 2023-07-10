747d400a7622cbdb69fc6a7ff8b1c1ff4b65fb89
baggiiiie <yingchao.dai22@gmail.com>
2023-07-10 21:09:59 +0800

    Updated rows

--- src/Row.js
+++ src/Row.js
@@ -1,9 +1,27 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from './axios';
 
-function Row(props) {
+function Row({title, fetchUrl, isLarge = false}) {
+    const [movies, setMovies] = useState([]);
+    const baseUrl = "https://image.tmdb.org/t/p/original/";
+
+    useEffect(()=>{
+        async function fetchData() {
+            const request = await axios.get(fetchUrl)
+            setMovies(request.data.results);
+            return request;
+        }
+        fetchData();
+    }, [fetchUrl])
+
     return (
-        <div>
-           <h1>This is a row</h1> 
+        <div className='row'>
+            <h2>{title}</h2>
+            {movies.map((movie) => {
+               <img src={`${baseUrl}${
+                    isLarge ? movie.poster_path : movie.backdrop_path
+                }`} alt={`${movie.name}`}/> 
+            })}
         </div>
     );
 }
