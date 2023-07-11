874eb48170095e73f364d2a8b232afdafe33cf79
baggiiiie <yingchao.dai22@gmail.com>
2023-07-11 15:20:57 +0800

    update router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,32 @@
 import React from 'react';
 import './App.css';
 import HomeScreen from './HomeScreen';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from 'react-router-dom';
 
 function App() {
   return (
     <div className="app">
-      <HomeScreen />
+       <Router>
+        {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+        <Routes>
+          <Route path='/' element={<HomeScreen/>} />
+
+          {/* <Route path="/" >
+            <HomeScreen />
+          </Route> */}
+          <Route path="/about">
+          </Route>
+          <Route path="/users">
+          </Route>
+          <Route path="/">
+          </Route>
+        </Routes>
+    </Router>
     </div>
   );
 }
