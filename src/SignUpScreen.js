0e38559192df8e1f2ca0907d4479a35884add18b
baggiiiie <yingchao.dai22@gmail.com>
2023-07-11 21:57:09 +0800

    updated sign up

--- src/SignUpScreen.js
+++ src/SignUpScreen.js
@@ -2,14 +2,24 @@ import React from 'react';
 import './SignUpScreen.css'
 
 function SignUpScreen(props) {
+    const register = (e) => {
+        e.preventDefualt()
+    }
+    const signIn = (e) => {
+        e.preventDefualt()
+    }
+
     return (
         <div className='signUpScreen'>
             <form >
                 <h1>Sign In</h1>
                 <input placeholder='Email' type='email' />
                 <input placeholder='Password' type='password' />
-                <button type='submit'>Sign In</button>
-                <h4>something here</h4>
+                <button type='submit' onClick={signIn}>Sign In</button>
+                <h4>
+                    <span className='signUpScreen_gray'>New to Netflix?</span>
+                    <span className='signUpScreen_link' onClick={register}>Sign up now!</span>
+                </h4>
             </form>
         </div>
     );
