068f927d25d364769c28a83ebb192aa80d324845
baggiiiie <yingchao.dai22@gmail.com>
2023-07-11 17:27:19 +0800

    updated sign up css

--- src/SignUpScreen.js
+++ src/SignUpScreen.js
@@ -4,10 +4,12 @@ import './SignUpScreen.css'
 function SignUpScreen(props) {
     return (
         <div className='signUpScreen'>
-            <form>
+            <form >
                 <h1>Sign In</h1>
                 <input placeholder='Email' type='email' />
                 <input placeholder='Password' type='password' />
+                <button type='submit'>Sign In</button>
+                <h4>something here</h4>
             </form>
         </div>
     );
