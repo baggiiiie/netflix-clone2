4e894e8337bb3e6775a689d9b94b36c88330db63
baggiiiie <yingchao.dai22@gmail.com>
2023-07-13 23:26:31 +0800

    updated userslice

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './App.css';
 import HomeScreen from './HomeScreen';
 import {
@@ -6,10 +6,28 @@ import {
   Routes,
   Route,
 } from 'react-router-dom';
+import { Auth, onAuthStateChanged } from 'firebase/auth';
 import LoginScreen from './LoginScreen';
+import { useDispatch } from 'react-redux';
+import { logout } from './features/counter/userSlice';
 
 function App() {
   const user = null;
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged((userAuth) => {
+      if (userAuth) {
+        // Logged in 
+        console.log(userAuth)
+      } else {
+        // Logged out
+        dispatch(logout)
+      }
+    })
+
+    return unsubscribe;
+  }, []);
 
   return (
     <div className="app">
