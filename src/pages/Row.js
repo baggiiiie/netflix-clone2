066afeed03820e80e423a8c63de56cf8f4731fb8
baggiiiie <yingchao.dai22@gmail.com>
2023-08-29 15:41:52 +0800

    updated hover scaling effect, first element translateX

--- src/pages/Row.js
+++ src/pages/Row.js
@@ -1,48 +1,51 @@
-import React, { useEffect, useState } from 'react';
-import axios from '../axios';
-import './Row.css';
+import React, { useEffect, useState } from "react";
+import axios from "../axios";
+import "./Row.css";
 
 function Row({ title, fetchUrl, isLarge = false }) {
-    const [movies, setMovies] = useState([]);
-    const baseUrl = "https://image.tmdb.org/t/p/original/";
+	const [movies, setMovies] = useState([]);
+	const baseUrl = "https://image.tmdb.org/t/p/original/";
 
-    useEffect(() => {
-        async function fetchData() {
-            const request = await axios.get(fetchUrl);
-            setMovies(request.data.results);
-            return request;
-        }
-        fetchData();
-    }, [fetchUrl]);
+	useEffect(() => {
+		async function fetchData() {
+			const request = await axios.get(fetchUrl);
+			setMovies(request.data.results);
+			return request;
+		}
+		fetchData();
+	}, [fetchUrl]);
 
-    return (
-        <div className='row'>
-            <h3 className='row_title'>{title}</h3>
-            <div className='row_posters'>
-                {movies.map(
-                    movie => (
-                        ((isLarge && movie.poster_path) ||
-                            (!isLarge && movie.backdrop_path)) && (
-                            <div
-                                key={movie.id}
-                                className={`row_poster ${ isLarge && 'row_posterLarge' }`}>
-                                <img
-                                    className='row_posterLogo'
-                                    src='https://upload.wikimedia.org/wikipedia/commons/7/7a/Logonetflix.png'
-                                />
-                                <img
-                                    className='row_posterPic'
-                                    src={`${ baseUrl }${ isLarge ? movie.poster_path : movie.backdrop_path
-                                        }`}
-                                    alt={`${ movie.name }`} />
-                            </div>
-
-                        )
-                    )
-                )}
-            </div>
-        </div>
-    );
+	return (
+		<div className="row">
+			<h3 className="row_title">{title}</h3>
+			<div className="row_posters">
+				{movies.map(
+					(movie, index) =>
+						((isLarge && movie.poster_path) ||
+							(!isLarge && movie.backdrop_path)) && (
+							<div
+								key={movie.id}
+								className={`row_poster ${isLarge && "row_posterLarge"} ${
+									index === 0 && "row_poster_first"
+								}`}
+							>
+								<img
+									className="row_posterLogo"
+									src="https://upload.wikimedia.org/wikipedia/commons/7/7a/Logonetflix.png"
+								/>
+								<img
+									className="row_posterPic"
+									src={`${baseUrl}${
+										isLarge ? movie.poster_path : movie.backdrop_path
+									}`}
+									alt={`${movie.name}`}
+								/>
+							</div>
+						)
+				)}
+			</div>
+		</div>
+	);
 }
 
 export default Row;
