9581bc750c52ee611c2e93aa4eac96d15045a7f6
baggiiiie <yingchao.dai22@gmail.com>
2023-09-10 16:40:03 +0800

    updated css

--- src/App.js
+++ src/App.js
@@ -1,70 +1,52 @@
-import React, { useEffect } from 'react';
-import './App.css';
-import HomeScreen from './pages/HomeScreen';
-import ProfileScreen from './pages/ProfileScreen';
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-} from 'react-router-dom';
-import { auth } from './firebase';
-import LoginScreen from './pages/LoginScreen';
-import { useDispatch, useSelector } from 'react-redux';
-import { logout, login, selectUser } from './features/counter/userSlice';
+import React, { useEffect } from "react";
+import "./App.css";
+import HomeScreen from "./pages/HomeScreen";
+import ProfileScreen from "./pages/ProfileScreen";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { auth } from "./firebase";
+import LoginScreen from "./pages/LoginScreen";
+import { useDispatch, useSelector } from "react-redux";
+import { logout, login, selectUser } from "./features/counter/userSlice";
 
 function App() {
-  const user = useSelector(selectUser);
-  const dispatch = useDispatch();
+	const user = useSelector(selectUser);
+	const dispatch = useDispatch();
 
-  useEffect(() => {
-    const unsubscribe = auth.onAuthStateChanged((user) => {
-      if (user) {
-        // Logged in 
-        console.log(user);
-        dispatch(login({
-          uid: user.uid,
-          email: user.email
-        }));
-      } else {
-        // Logged out
-        dispatch(logout());
-      }
-    });
+	useEffect(() => {
+		const unsubscribe = auth.onAuthStateChanged((user) => {
+			if (user) {
+				// Logged in
+				// console.log(user);
+				dispatch(
+					login({
+						uid: user.uid,
+						email: user.email,
+					})
+				);
+			} else {
+				// Logged out
+				dispatch(logout());
+			}
+		});
 
-    return unsubscribe;
-  }, [dispatch]);
+		return unsubscribe;
+	}, [dispatch]);
 
-  return (
-    <div className="app">
-      {/* <Router>
-        <Routes>
-          {(!user) ? (
-            <Route path='/' element={<LoginScreen />} />
-          ) : (
-            <>
-              <Route path='/' element={<HomeScreen />} />
-              <Route path='/profile' element={<ProfileScreen />} />
-            </>
-            )}
-        </Routes>
-      </Router> */}
-      <Router>
-        {!user ? (
-          <LoginScreen />
-        ) : (
-          <Routes>
-            <Route path='/' element={
-              <HomeScreen />} />
-            <Route path="/profile" element={
-              <ProfileScreen />
-            }>
-            </Route>
-            <Route path="/users">
-            </Route>
-          </Routes>)}
-      </Router>
-    </div>
-  );
+	return (
+		<div className="app">
+			<Router>
+				{!user ? (
+					<LoginScreen />
+				) : (
+					<Routes>
+						<Route path="/" element={<HomeScreen />} />
+						<Route path="/profile" element={<ProfileScreen />}></Route>
+						<Route path="/users"></Route>
+					</Routes>
+				)}
+			</Router>
+		</div>
+	);
 }
 
 export default App;
